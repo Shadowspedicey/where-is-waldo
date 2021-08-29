127260c0379a852f568c5598ed7b976a1aa9a27b
Ahmed Khaled <shadowspedicey@gmail.com>
2021-08-29 09:37:01 +0200

    Made the list of characters random everytime

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "regenerator-runtime/runtime";
 import ClickMenu from "./modules/ClickMenu";
 import "./global.css";
 import "./style.css";
@@ -5,21 +6,14 @@ import AD_2222 from "./levels/AD-2222.jpg";
 import data from "./data/AD-2222.json";
 
 document.querySelector("img").src = AD_2222;
-window.addEventListener("click", (e) =>	console.log(e.x / e.target.offsetWidth * 1000, e));
-window.addEventListener("click", (e) =>
-{
-	if (document.querySelector("#click-menu")) return ClickMenu.close();
-	ClickMenu.open(e, data);
-});
+
+const shuffled = data.sort(() => 0.5 - Math.random());
+let newData = shuffled.slice(0, 3);
+
+window.addEventListener("click", (e) =>	console.log(Math.round(e.pageX / e.target.offsetWidth * 1000), Math.round(e.pageY / e.target.height * 1000), e));
+window.addEventListener("click", (e) => document.querySelector("#click-menu") ? ClickMenu.close() : ClickMenu.open(e, "AD-2222", newData));
 
 const creator = document.createElement("a");
 creator.href = "https://anomaly-world.com/";
 creator.textContent = "Anamoly World";
 document.querySelector("#level-cp span").appendChild(creator);
-
-// Don't starve guy
-// Spider-Man
-// Tom from tom and jerry
-// Dragonborn
-// Sonic
-// Connor from cyberlife
